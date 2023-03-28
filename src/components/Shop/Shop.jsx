95fe21a972ac3b3178a44ee59695adc4ffc2aed9
srmahmud2 <srmahmud2@gmail.com>
2023-03-28 13:56:54 +0100

    retrived product form stored and set it to cart state

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -16,8 +16,24 @@ const Shop = () => {
   //add cart info to local storage
   useEffect(() => {
     const storedCart = getShoppingCart();
-    console.log(storedCart);
-  }, []);
+    let savedCart = [];
+
+    //step 1: get the id of the added product
+    for (const id in storedCart) {
+      //step 2: get the product from the products stat
+      const addedProduct = products.find((product) => product.id === id);
+      if (addedProduct) {
+        //step 3: add quantity
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        //step 4: add the product to the savedCart
+        savedCart.push(addedProduct);
+      }
+      // console.log("added product", addedProduct);
+    }
+    // step 5: save the cart
+    setCart(savedCart);
+  }, [products]);
 
   //declare handleAddToCart
   const handleAddToCart = (product) => {
